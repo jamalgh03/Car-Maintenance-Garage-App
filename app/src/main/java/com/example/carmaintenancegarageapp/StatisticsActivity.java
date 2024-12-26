02b20251bad4d4d30a76015c5c26463ff694f68f
Ahmad Yahya <Ahmad7122001@gmail.com>
2024-12-26 19:26:12 +0200

    log in ,sign up,profile,welcome page ,statistics   V2

---
+++ app/src/main/java/com/example/carmaintenancegarageapp/StatisticsActivity.java
@@ -0,0 +1,42 @@
+package com.example.carmaintenancegarageapp;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.statistics_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.export_report) {
+            // Handle export
+            return true;
+        } else if (id == R.id.settings) {
+            // Handle settings
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
