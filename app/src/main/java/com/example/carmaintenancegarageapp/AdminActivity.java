72d248e89601231b464197d4dcb896e470f58abd
Yazan <yazan5sulaiman@gmail.com>
2024-12-31 15:00:02 +0200

    test

--- app/src/main/java/com/example/carmaintenancegarageapp/AdminActivity.java
+++ app/src/main/java/com/example/carmaintenancegarageapp/AdminActivity.java
@@ -1,6 +1,11 @@
 package com.example.carmaintenancegarageapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +13,67 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdminActivity extends AppCompatActivity {
+    LinearLayout pending_filed;
+    ImageButton add_request;
+    ImageButton edit_request_admin;
+    ImageButton add_car_admin;
+    ImageButton add_new_car_manufactor_admin;
+    ImageButton stats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        pending_filed = findViewById(R.id.pendingfiled);
+        add_request = findViewById(R.id.addrequestadmin);
+        edit_request_admin = findViewById(R.id.editrequestadmin);
+        add_car_admin = findViewById(R.id.addcaradmin);
+        add_new_car_manufactor_admin = findViewById(R.id.addnewcarmanufactoradmin);
+        stats = findViewById(R.id.stats);
+        add_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ig = new Intent(AdminActivity.this, AddRequestAdmin.class);
+                startActivity(ig);
 
+            }
+        });
+        edit_request_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ig = new Intent(AdminActivity.this, EditRequest.class);
+                startActivity(ig);
+
+            }
+        });
+        add_car_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ig = new Intent(AdminActivity.this, AddCar.class);
+                startActivity(ig);
+
+            }
+        });
+        add_new_car_manufactor_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ig = new Intent(AdminActivity.this, AddNewCarManufacturer.class);
+                startActivity(ig);
+
+            }
+        });
+
+        stats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ig = new Intent(AdminActivity.this, StatisticsActivity.class);
+                startActivity(ig);
+
+            }
+        });
     }
 }
